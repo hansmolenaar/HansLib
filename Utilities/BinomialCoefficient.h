9dc747eff657da80c85ebf3bc7362362e9040ed5
Hans Molenaar <molenaar.johannes@gmail.com>
2022-12-11 11:01:18 +0100

    Use templates

--- Utilities/BinomialCoefficient.h
+++ Utilities/BinomialCoefficient.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "Factorial.h"
 
 namespace BinomialCoefficient
 {
-   int Get(int n, int k);
+   template <typename T>
+   T Get(T n, T k);
+}
+
+template <typename T>
+T BinomialCoefficient::Get(T n, T k)
+{
+   BoundsCheck<T>::Create(0, n).check(k);
+   T result = 1;
+   for (T m = 0; m < k; ++m)
+   {
+      result *= (n - m);
+   }
+   return result / Factorial::Get(k);
 }
--- Utilities/Factorial.h
+++ Utilities/Factorial.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "BoundsCheck.h"
 
 namespace Factorial
 {
-   // Use larger type?
-   int Get(int);
+   template <typename T>
+   T Get(T);
+}
+
+template <typename T>
+T Factorial::Get(T number)
+{
+   BoundsCheck<T>::CreateIsNonNegative().check(number);
+   T result = 1;
+   for (T n = 1; n <= number; ++n)
+   {
+      result *= n;
+   }
+   return result;
 }
