4281d624d9a360cffb88ab6bbaee89cbea04c9b7
Hans Molenaar <molenaar.johannes@gmail.com>
2023-02-12 11:28:35 +0100

    Squared hat

--- Functions/MultiVariableFunctionExamples.cpp
+++ Functions/MultiVariableFunctionExamples.cpp
@@ -1,6 +1,49 @@
 #include "MultiVariableFunctionExamples.h"
 #include "MultiVariablePolynomial.h"
 #include "MultiVariableRealValuedFunctionNoDerivatives.h"
+#include "SingleVariableFunctionExamples.h"
+#include "Utilities/Defines.h"
+#include <numeric>
+
+
+namespace
+{
+   class  SkewedHatSquaredEval : public IMultiVariableFunctionEvaluate
+   {
+   public:
+      SkewedHatSquaredEval(const std::vector<double>& pos);
+
+      int getDimension() const override;
+      double operator()(std::span<const double> x) const;
+   private:
+      std::vector<std::unique_ptr<ISingleVariableRealValuedFunction>> m_hats;
+   };
+
+   SkewedHatSquaredEval::SkewedHatSquaredEval(const std::vector<double>& pos) : m_hats(pos.size())
+   {
+      str::transform(pos, m_hats.begin(), [](double x) {return SingleVariableFunctionExamples::GetSkewedtHatFunction(x); });
+   }
+
+   int SkewedHatSquaredEval::getDimension() const
+   {
+      return static_cast<int>(m_hats.size());
+   }
+
+   double SkewedHatSquaredEval::operator()(std::span<const double> x) const
+   {
+      if (x.size() != m_hats.size())
+      {
+         throw MyException("SkewedHatSquaredEval::operator dimension problem");
+      }
+      double result = 0;
+      for (size_t n = 0; n < x.size(); ++n)
+      {
+         const double eval = m_hats.at(n)->Evaluate(x[n]);
+         result += eval * eval;
+      }
+      return result / x.size();
+   }
+}
 
 std::unique_ptr<IMultiVariableRealValuedFunction> MultiVariableFunctionExamples::GetPolynomial(const std::vector< std::pair<std::vector<int>, double>>& terms)
 {
@@ -30,4 +73,16 @@ std::vector<Interval<double>> MultiVariableFunctionExamples::TridBounds(int dim)
 {
    const Interval<double> intv(-dim * dim, dim * dim);
    return { static_cast<size_t>(dim), intv };
+}
+
+MultiVariableFunctionExamples::Examplefunction MultiVariableFunctionExamples::SkewedHatSquared(const std::vector<double>& pos)
+{
+   const int dim = static_cast<int>(pos.size());
+   Examplefunction result;
+   result.Function = std::make_unique<MultiVariableRealValuedFunctionNoDerivatives>(dim, std::make_unique<SkewedHatSquaredEval>(pos));
+   result.Domain = std::vector<Interval<double>>(pos.size(), Interval<double>(0, 1));
+   result.Maximum = std::make_unique<ExampleExtremumInfo>();
+   result.Maximum->Extremum = 1.0;
+   result.Maximum->Positions = std::vector<std::vector<double>>{ std::vector<double>(pos.begin(), pos.end()) };
+   return result;
 }
--- Functions/MultiVariableFunctionExamples.h
+++ Functions/MultiVariableFunctionExamples.h
@@ -5,9 +5,25 @@
 #include <memory>
 #include <vector>
 #include <functional>
+#include <limits>
 
 namespace MultiVariableFunctionExamples
 {
+   struct ExampleExtremumInfo
+   {
+      double Extremum = std::numeric_limits<double>::quiet_NaN();
+      std::vector<std::vector<double>> Positions;
+   };
+
+   struct Examplefunction
+   {
+      std::unique_ptr<IMultiVariableRealValuedFunction> Function;
+      std::vector<Interval<double>> Domain;
+      std::unique_ptr<ExampleExtremumInfo> Minimum;
+      std::unique_ptr<ExampleExtremumInfo> Maximum;
+   };
+
+
    std::unique_ptr<IMultiVariableRealValuedFunction> GetPolynomial(const std::vector< std::pair<std::vector<int>, double>>&);
    std::unique_ptr<IMultiVariableRealValuedFunction> SixHumpCamelFunction();
    std::unique_ptr<IMultiVariableRealValuedFunction> ScaleInput(
@@ -15,4 +31,6 @@ namespace MultiVariableFunctionExamples
       std::unique_ptr<std::function<std::vector<double>(const std::vector<double>&)>>&&);
    std::unique_ptr<IMultiVariableRealValuedFunction> TridFunction(int dim);
    std::vector<Interval<double>> TridBounds(int dim);
+
+   Examplefunction SkewedHatSquared(const std::vector<double>& pos);
 }
