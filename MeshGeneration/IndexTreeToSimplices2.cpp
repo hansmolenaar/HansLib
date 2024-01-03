0018771169207b0496d4b09a78b3c9094d76c0d0
hansmolenaar <molenaar.johannes@gmail.com>
2024-01-03 21:30:00 +0100

    Triangulation of index tree in 2D

--- MeshGeneration/IndexTreeToSimplices2.cpp
+++ MeshGeneration/IndexTreeToSimplices2.cpp
@@ -14,10 +14,32 @@ namespace
       std::vector<std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners>> Triangles;
    };
 
+   std::pair<RatPoint2, RatPoint2> GetOrientedEdge(const IntervalTree::AdjacentDirection& dir, const std::array<RatPoint2, 4> corners)
+   {
+      if (dir == IntervalTree::AdjacentDirection{ 0, true })
+      {
+         return std::make_pair(corners.at(1), corners.at(2));
+      }
+      if (dir == IntervalTree::AdjacentDirection{ 0, false })
+      {
+         return std::make_pair(corners.at(3), corners.at(0));
+      }
+      if (dir == IntervalTree::AdjacentDirection{ 1, true })
+      {
+         return std::make_pair(corners.at(2), corners.at(3));
+      }
+      if (dir == IntervalTree::AdjacentDirection{ 1, false })
+      {
+         return std::make_pair(corners.at(0), corners.at(1));
+      }
+      throw MyException("GetOrientedEdge should not come here");
+   }
+
    void ActionSplit::operator()(const IntervalTree::Index<2>& index)
    {
+      const BoundingBox<Rational, 2> bb = index.getBbOfCell();
       const auto& neighbors = IntervalTree::GetAdjacentNeighbors2();
-      std::array<bool, 4> moreRefined{false, false, false, false};
+      std::array<bool, 4> moreRefined{ false, false, false, false };
       size_t pos = 0;
       for (const auto& dir : neighbors)
       {
@@ -33,7 +55,7 @@ namespace
          ++pos;
       }
 
-      const BoundingBox<Rational, 2> bb = index.getBbOfCell();
+     
       const auto lwr = bb.getLower();
       const auto upr = bb.getUpper();
       const RatPoint2 ll(lwr[0], lwr[1]);
@@ -41,17 +63,38 @@ namespace
       const RatPoint2 uu(upr[0], upr[1]);
       const RatPoint2 lu(lwr[0], upr[1]);
 
-      if (str::none_of(moreRefined, [](bool b) {return b;}))
+      if (str::none_of(moreRefined, [](bool b) {return b; }))
       {
          Triangles.emplace_back(std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners >{ ll, ul, uu });
          Triangles.emplace_back(std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners >{ ll, uu, lu });
+      }
+      else
+      {
+         const std::array<RatPoint2, 4> corners{ ll, ul, uu, lu };
+         const RatPoint2 center = index.getCenter();
+         pos = 0;
+         for (const auto& dir : neighbors)
+         {
+            const auto edge = GetOrientedEdge(dir, corners);
+            if (!moreRefined.at(pos))
+            {
+               Triangles.emplace_back(std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners >{center, edge.first, edge.second });
+            }
+            else
+            {
+               auto midEdge = (edge.first + edge.second) * Rational{ 1,2 };
+               Triangles.emplace_back(std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners >{center, edge.first, midEdge });
+               Triangles.emplace_back(std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners >{center, midEdge, edge.second });
+            }
+            ++pos;
+         }
       }
    }
 }
 
 std::vector<std::array<RatPoint2, ReferenceShapePolygon::TriangleNumCorners>> IndexTreeToSimplices2::Create(const IntervalTree::IndexTree<2>& tree)
 {
-   ActionSplit action{tree};
+   ActionSplit action{ tree };
    tree.foreachLeaf(action);
    return action.Triangles;
 }
--- MeshGenerationTest/IndexTreeToSimplices2Test.cpp
+++ MeshGenerationTest/IndexTreeToSimplices2Test.cpp
@@ -4,6 +4,7 @@
 #include "IntervalTreeRefinePredicate.h"
 #include "Paraview.h"
 #include "IntervalTreeBalance.h"
+#include "IntervalTreeVtk.h"
 
 using namespace IntervalTree;
 
@@ -50,13 +51,25 @@ TEST(IndexTreeToSimplices2Test, RefinedToVtk_1)
    IndexTree<dim> tree;
    tree.refineUntilReady(doRefine);
    IntervalTree::Balance(tree);
-
+   //Paraview::Write("IndexTreeToSimplices2Test_RefinedToVtk_1_base", *IntervalTree::GetVtkData(tree));
    const auto triangles = IndexTreeToSimplices2::Create(tree);
-   //ASSERT_EQ(8, triangles.size());
+   std::set<std::pair<RatPoint2, RatPoint2>> directedEdges;
+   for (const auto& triangle : triangles)
+   {
+      const auto edge0 = std::make_pair(triangle.at(0), triangle.at(1));
+      const auto edge1 = std::make_pair(triangle.at(1), triangle.at(2));
+      const auto edge2 = std::make_pair(triangle.at(2), triangle.at(0));
+      const std::array<std::pair<RatPoint2, RatPoint2>, 3> edges{edge0, edge1, edge2};
+      for (const auto& e : edges)
+      {
+         ASSERT_FALSE(directedEdges.contains(e));
+         directedEdges.insert(e);
+      }
+   }
 
    const auto vtkData = IndexTreeToSimplices2::ToVtkData(triangles);
-   //ASSERT_EQ(8, vtkData->getNumCells());
-   //ASSERT_EQ(9, vtkData->getNumNodes());
-   Paraview::Write("IndexTreeToSimplices2Test_RefinedToVtk_1", *vtkData);
+   ASSERT_EQ(90, vtkData->getNumCells());
+   ASSERT_EQ(54, vtkData->getNumNodes());
+   //Paraview::Write("IndexTreeToSimplices2Test_RefinedToVtk_1", *vtkData);
 }
 
