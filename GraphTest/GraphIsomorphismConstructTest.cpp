df3229ac0a2ded7e3fb54f697a96a64f1071445e
hansmolenaar <Molenaar.Johannes@gmail.com>
2025-11-03 17:58:46 +0100

    One more test

--- GraphTest/GraphIsomorphismConstructTest.cpp
+++ GraphTest/GraphIsomorphismConstructTest.cpp
@@ -7,6 +7,7 @@
 #include "UndirectedGraphLibrary.h"
 
 using namespace GraphIsomorphism;
+using namespace Graph;
 
 namespace
 {
@@ -36,26 +37,15 @@ TEST(GraphIsomorphismConstructTest, DegreePath3)
 
 TEST(GraphIsomorphismConstructTest, DegreeStar123)
 {
-    const auto graphs =
-        std::make_pair(UndirectedGraphLibrary::Get_Star({1, 2, 3}), UndirectedGraphLibrary::Get_Star({3, 1, 2}));
+    const auto graphs = std::make_pair(UndirectedGraphLibrary::Get_Star({1, 2, 3}), UndirectedGraphLibrary::Get_Star({3, 1, 2}));
     const auto status = Construct{}.actionConnected(*graphs.first, *graphs.second);
     ASSERT_EQ(status.getFlag(), Flag::Isomorphic);
 }
 
-// TODO
-#if false
-
 TEST(GraphIsomorphismConstructTest, DegreePan3)
 {
-    const auto graph = UndirectedGraphFromG6::Create(UndirectedGraphFromG6::pan3);
-    const auto permuted =
-        UndirectedGraph::CreatePermuted(*graph, Permutation::Create(std::vector<Permutation::Entry>{2, 1, 0, 3}));
-    const auto graphs = std::make_pair(*graph, permuted);
-    const auto taggers = std::make_pair(TaggerDegree(graphs.first), TaggerDegree(graphs.second));
-    const auto retval = TagCompare{}(taggers);
-    ASSERT_EQ(retval.TagCompareStatus, TagCompare::Result::TagStatus::Equivalent);
-    ASSERT_EQ(retval.VertexPairs.size(), 2);
-    ASSERT_EQ(retval.VertexPairs.back(), (VertexPair{2, 0}));
-    ASSERT_EQ(retval.VertexPairs.front(), (VertexPair{3, 3}));
+    const auto graph = UndirectedGraphFromG6::CreateConnected(UndirectedGraphFromG6::pan3);
+    const auto permuted = GraphUsc::CreatePermuted(*graph, Permutation::Create(std::vector<Permutation::Entry>{2, 1, 0, 3}));
+    const auto status = Construct{}.actionConnected(*graph, permuted);
+    ASSERT_EQ(status.getFlag(), Flag::Isomorphic);
 }
-#endif
