e9b67040c494faf3140408ec53cd8f2f95d27d4d
hansmolenaar <Molenaar.Johannes@gmail.com>
2026-01-27 14:44:50 +0100

    More testing

--- GraphTest/GraphIsomorphismIGraphCompareTest.cpp
+++ GraphTest/GraphIsomorphismIGraphCompareTest.cpp
@@ -15,30 +15,30 @@ using namespace Utilities;
 
 namespace
 {
-#if false
- std::vector<std::unique_ptr<IGraphCompare>> CreateComparers(IGraphCompareFactory& factory, const std::vector<std::unique_ptr<IGraphUs>>& graphs)
+
+std::vector<std::unique_ptr<IGraphCompare>> CreateComparers(IGraphCompareFactory &factory,
+                                                            const std::vector<std::unique_ptr<IGraphUs>> &graphs)
 {
     std::vector<std::unique_ptr<IGraphCompare>> result;
     str::transform(graphs, std::back_inserter(result),
                    [&factory](const auto &upg) { return factory.createGraphCompare(*upg); });
- return result;
+    return result;
 }
-#endif
 
-void CheckSelfConsistency(IGraphCompareFactory &factory, const IGraphUs &graph)
+void CheckSelfConsistency(IGraphCompareFactory &factory, const IGraphCompare &compare)
 {
     const Permutation::Entry numPermutations = 5;
-    const auto compare = factory.createGraphCompare(graph);
-    ASSERT_EQ(compare->getGraph().getNumVertices(), graph.getNumVertices());
-    ASSERT_EQ(compare->compareGraph(*compare), std::weak_ordering::equivalent);
-    const auto grouping = compare->getVertexGrouping();
+    const auto &graph = compare.getGraph();
+    ASSERT_EQ(compare.getGraph().getNumVertices(), graph.getNumVertices());
+    ASSERT_EQ(compare.compareGraph(compare), std::weak_ordering::equivalent);
+    const auto grouping = compare.getVertexGrouping();
     const auto groupSizes = CondenseSizeSequence(grouping.getGroupSizes());
     for (auto n : Iota::GetRange(numPermutations))
     {
         const UndirectedGraph graphPermuted = UndirectedGraph::CreateRandomShuffled(graph, n);
         const auto comparerPermuted = factory.createGraphCompare(graphPermuted);
-        ASSERT_EQ(compare->compareGraph(*comparerPermuted), std::weak_ordering::equivalent);
-        ASSERT_EQ(comparerPermuted->compareGraph(*compare), std::weak_ordering::equivalent);
+        ASSERT_EQ(compare.compareGraph(*comparerPermuted), std::weak_ordering::equivalent);
+        ASSERT_EQ(comparerPermuted->compareGraph(compare), std::weak_ordering::equivalent);
 
         const auto groupingPermuted = comparerPermuted->getVertexGrouping();
         ASSERT_EQ(grouping.countUnique(), groupingPermuted.countUnique());
@@ -48,11 +48,33 @@ void CheckSelfConsistency(IGraphCompareFactory &factory, const IGraphUs &graph)
     }
 }
 
-void CheckConsistencyList(IGraphCompareFactory &factory, const std::vector<std::unique_ptr<IGraphUs>> &graphs)
+void CheckSymmetry(const IGraphCompare &compare1, const IGraphCompare &compare2)
 {
-    for (const auto &ug : graphs)
+    const auto cmp = compare1.compareGraph(compare2);
+    if (cmp == std::weak_ordering::equivalent)
     {
-        CheckSelfConsistency(factory, *ug);
+        ASSERT_EQ(compare2.compareGraph(compare1), std::weak_ordering::equivalent);
+        ASSERT_EQ(compare1.getVertexGrouping()(), compare2.getVertexGrouping()());
+    }
+    else if (cmp == std::weak_ordering::less)
+    {
+        ASSERT_EQ(compare2.compareGraph(compare1), std::weak_ordering::greater);
+    }
+    else
+    {
+        ASSERT_EQ(compare2.compareGraph(compare1), std::weak_ordering::less);
+    }
+}
+
+void CheckConsistencyList(IGraphCompareFactory &factory, const std::vector<const IGraphCompare *> &comparers)
+{
+    for (const auto *cmp1 : comparers)
+    {
+        CheckSelfConsistency(factory, *cmp1);
+        for (const auto *cmp2 : comparers)
+        {
+            CheckSymmetry(*cmp1, *cmp2);
+        }
     }
 }
 
@@ -67,5 +89,7 @@ void GraphTest::CheckComparerBasics(GraphIsomorphism::IGraphCompareFactory &fact
     graphs.emplace_back(UndirectedGraphLibrary::Get_Path(2));
     graphs.emplace_back(UndirectedGraphLibrary::Get_DisconnectedGraph(2));
 
-    CheckConsistencyList(factory, graphs);
+    const auto comparers = CreateComparers(factory, graphs);
+
+    CheckConsistencyList(factory, getCastPointers<const IGraphCompare>(comparers));
 }
