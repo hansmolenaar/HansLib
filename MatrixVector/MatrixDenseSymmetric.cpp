73817f167a9035da6d27ebf3f8c4d2592aad8abe
hansmolenaar <Molenaar.Johannes@gmail.com>
2026-02-12 18:51:19 +0100

    times vector symmetric case

--- MatrixVector/MatrixDenseSymmetric.cpp
+++ MatrixVector/MatrixDenseSymmetric.cpp
@@ -1,6 +1,7 @@
 #include "MatrixDenseSymmetric.h"
 
 #include "IMatrixUtils.h"
+#include "Iota.h"
 
 MatrixDenseSymmetric::MatrixDenseSymmetric(int dim) : m_dim(dim), m_indexer(dim), m_matrix(m_indexer.getFlatSize(), 0.0)
 {
@@ -23,5 +24,23 @@ double &MatrixDenseSymmetric::operator()(int row, int col)
 
 void MatrixDenseSymmetric::timesVector(std::span<const double> vecin, std::span<double> result) const
 {
-    MatrixTimesVector(*this, vecin, result);
+    const auto dim = GetDimension();
+    Utilities::MyAssert(static_cast<int>(vecin.size()) == dim);
+    Utilities::MyAssert(static_cast<int>(result.size()) == dim);
+
+    // Diag
+    for (auto ind : Iota::GetRange(dim))
+    {
+        result[ind] = (*this)(ind, ind) * vecin[ind];
+    }
+
+    for (auto row : Iota::GetRange(dim))
+    {
+        for (auto col : Iota::GetRangeFromTo(row + 1, dim))
+        {
+            const double entry = (*this)(row, col);
+            result[row] += entry * vecin[col];
+            result[col] += entry * vecin[row];
+        }
+    }
 }
